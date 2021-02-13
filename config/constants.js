670436659f461a54ff6ff931b7bdecade9ba7a64
AndresGav <andresgavino11@hotmail.com>
2021-02-13 14:09:29 -0500

    Add Files to Conect database

---
+++ config/constants.js
@@ -0,0 +1,7 @@
+export const dbConfig = {
+    server: 'mainserverandres.database.windows.net',
+    port: 1433,
+    user: 'andresadmin',
+    password: 'GRANhtc9595',
+    database: 'ContacAG',
+ }
